0398f5dc016c930c9dabaf5b52f7faed06081127
olegthunder <albdgnv@gmail.com>
2023-01-11 14:22:07 +0500

    js file edit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let reverseInt = '';
+  for (let i = str.length; i > 0; i--) {
+    reverseInt = reverseInt + str[i - 1];
+  }
+  if (reverseInt[reverseInt.length - 1] == "-" ) {
+    reverseInt = reverseInt.slice(0,-1);
+  }
+  if (reverseInt[0] == "0" ) {
+    reverseInt = reverseInt.slice(0);
+  }
+  return reverseInt;
 }
